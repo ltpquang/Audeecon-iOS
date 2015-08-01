d475926e899c8ce0faf3a866724649adca66a9b9
Le Thai Phuc Quang <phuc.quang102@gmail.com>
2015-08-01 19:02:06 +0700

    App lifecycle handled

--- Audeecon/AppDelegate.m
+++ Audeecon/AppDelegate.m
@@ -61,7 +61,7 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    [self goOffline];
+    [self disconnect];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
@@ -75,7 +75,10 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [self goOnline];
+    [self connect];
+    if (self.currentUser != nil) {
+        //_xmppStream
+    }
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
@@ -101,6 +104,7 @@
     self.messagingCenter = nil;
     self.keyboardView = nil;
     self.stickerRecommender = nil;
+    self.currentUser = nil;
     [self clearTemperaryFolder];
 }
 
